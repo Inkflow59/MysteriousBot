7e76293625b530926bf495509aca841e946766a4
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2022-08-09 13:39:23 +0200

    lienHack.js addition

---
+++ assets/lienHack.js
@@ -0,0 +1,33 @@
+var sayHello = function() {
+    alert('Bienvenue a la frontiere du site Internet "Mysterious Bot"');
+};
+
+var sayHello = function() {
+    alert('Ici, tu vas pouvoir sortir et profiter de Google pour aller sur Internet');
+};
+
+sayHello = function() {
+    alert('Mais avant ...');
+};
+
+var sayHello = function() {
+    alert('Pour te parmettre de partir');
+};
+
+var sayHello = function() {
+    alert('Il me faut le mot de passe');
+};
+
+var password = prompt('Mot de passe :');
+
+var sayHello = function() {
+    alert('ALERTE AU BUG !!');
+};
+
+var sayHello = function() {
+    alert('ACTIVATION DU PLAN B');
+};
+
+var sayHello = function() {
+    alert('CHOISISSEZ LE BON MOT DE PASSE PARMI CES 20 PROPOSITIONS (CHERCHEZ DANS VOS DOSSIERS PASSWORD.ZIP ET DEZIPPEZ LE AVEC LES 2 MOTS DE PASSE CACHES DANS LE CODE DU SITE OU DANS LE TITRE DES ONGLETS, REFAITES L-AVENTURE POUR PLUS D-INFORMATIONS)');
+};
