51dc2d2d91647e95bbf80ba8b70d3d7b8c94acb6
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2022-08-10 13:39:18 +0200

    .vue file library uploaded

---
+++ JSMenu/mysteriousBot.vue
@@ -0,0 +1,28 @@
+<template>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MystLIB</title>
+<body>
+    <div id="zebi">Lancez Mysterious Bot en lançant <a href="..\index.html">index.html</a></div>
+</body>
+</template>
+
+<script>
+export default {
+    name: 'mysteriousBot',
+    data() {
+        return {
+            mysteriousBot=1
+            htmlFileName='index.html'
+            filePath='..\index.html'
+            launchScript='menuScript.js'
+        }
+    }
+}</script>
+
+<style>
+#background {
+
+}</style>
